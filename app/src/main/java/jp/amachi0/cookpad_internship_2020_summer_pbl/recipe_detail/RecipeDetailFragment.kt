6393b0f99fc60bfd6e1cd605875ca131c4db69a2
天知 <shunsuke.kmkz0617@gmail.com>
2020-08-28 17:30:57 +0900

    レシピ詳細画面の枠を追加（未実装）

---
+++ app/src/main/java/jp/amachi0/cookpad_internship_2020_summer_pbl/recipe_detail/RecipeDetailFragment.kt
@@ -0,0 +1,7 @@
+package jp.amachi0.cookpad_internship_2020_summer_pbl.recipe_detail
+
+import androidx.fragment.app.Fragment
+import jp.amachi0.cookpad_internship_2020_summer_pbl.R
+
+class RecipeDetailFragment : Fragment(R.layout.fragment_recipe_detail) {
+}
---
+++ app/src/main/java/jp/amachi0/cookpad_internship_2020_summer_pbl/recipe_detail/RecipeDetailViewModel.kt
@@ -0,0 +1,3 @@
+package jp.amachi0.cookpad_internship_2020_summer_pbl.recipe_detail
+
+class RecipeDetailViewModel
